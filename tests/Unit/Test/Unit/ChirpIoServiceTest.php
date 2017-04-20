7884aa89ac92ba83d2ef7d29ded7acf06f0692c5
Jessica Mauerhan <jessicamauerhan@gmail.com>
2017-04-20 11:02:57 -0500

    failing test for transformer collaboration

--- tests/Unit/Test/Unit/ChirpIoServiceTest.php
+++ tests/Unit/Test/Unit/ChirpIoServiceTest.php
@@ -3,20 +3,44 @@
 namespace Test\Unit;
 
 use Chirper\Chirp\ChirpIoService;
+use Chirper\Chirp\ChirpPersistenceDriver;
+use Chirper\Chirp\JsonChirpTransformer;
+use Chirper\Http\Request;
 use PHPUnit\Framework\TestCase;
 
 class ChirpIoServiceTest extends TestCase
 {
-    public function testCreateSendsJsonToTransformer(){
+    public function testCreateSendsJsonToTransformer()
+    {
+        $json        = "{}";
+        $transformer = $this->createMock(JsonChirpTransformer::class);
+        $transformer->expects($this->once())
+                    ->method('toChirp')
+                    ->with($json);
 
+        $persistenceDriver = $this->createMock(ChirpPersistenceDriver::class);
+
+        $service = new ChirpIoService($transformer, $persistenceDriver);
+
+        $request = new Request('POST', 'chirp', [], $json);
+
+        $service->create($request);
     }
 
-    public function testCreateReturnsInvalidChirpResponseWhenTransformerThrowsException(){}
-
-    public function testCreateSendsChirpToDatabaseDriver(){}
-
-    public function testCreateReturnsInternalErrorResponseWhenDatabaseDriverThrowsException(){}
-
-    public function testCreateReturnsChirpCreatedResponse(){}
+//    public function testCreateReturnsInvalidChirpResponseWhenTransformerThrowsException()
+//    {
+//    }
+//
+//    public function testCreateSendsChirpToDatabaseDriver()
+//    {
+//    }
+//
+//    public function testCreateReturnsInternalErrorResponseWhenDatabaseDriverThrowsException()
+//    {
+//    }
+//
+//    public function testCreateReturnsChirpCreatedResponse()
+//    {
+//    }
 
 }
