3a58485dd196e041dfb74145a1abfebce3461cfd
Jessica Mauerhan <jessicamauerhan@gmail.com>
2017-04-20 10:07:51 -0500

    Given I write a chirp

--- features/bootstrap/Api/CreateChirpContext.php
+++ features/bootstrap/Api/CreateChirpContext.php
@@ -4,16 +4,24 @@ namespace Test\Behavior\Context\Api;
 
 use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
+use Faker;
 
 class CreateChirpContext implements Context
 {
+
+    private $faker;
+
+    public function __construct()
+    {
+        $this->faker = Faker\Factory::create();
+    }
 
     /**
      * @Given I write a Chirp with :numCharacters or less characters
      */
     public function iWriteAChirpWithOrLessCharacters($numCharacters)
     {
-        throw new PendingException();
+        $text = $this->faker->realText($numCharacters);
     }
 
     /**
