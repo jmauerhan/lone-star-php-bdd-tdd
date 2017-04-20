a07b10fbe2e297e475aefce41e5d7852cdb0f101
Jessica Mauerhan <jessicamauerhan@gmail.com>
2017-04-20 10:17:42 -0500

    post the chirp

--- features/bootstrap/Api/CreateChirpContext.php
+++ features/bootstrap/Api/CreateChirpContext.php
@@ -5,15 +5,31 @@ namespace Test\Behavior\Context\Api;
 use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Faker;
+use GuzzleHttp\Client;
 
 class CreateChirpContext implements Context
 {
 
+    /** @var Faker\Generator */
     private $faker;
+
+    /** @var Client */
+    private $httpClient;
+
+    /** @var string */
+    private $chirpText;
+
+    /** @var string */
+    private $uuid;
 
     public function __construct()
     {
-        $this->faker = Faker\Factory::create();
+        $this->faker      = Faker\Factory::create();
+        $clientOpt        = [
+            'base_uri' => 'http://localhost:3000',
+        ];
+        $client           = new Client($clientOpt);
+        $this->httpClient = $client;
     }
 
     /**
@@ -21,7 +37,7 @@ class CreateChirpContext implements Context
      */
     public function iWriteAChirpWithOrLessCharacters($numCharacters)
     {
-        $text = $this->faker->realText($numCharacters);
+        $this->chirpText = $this->faker->realText($numCharacters);
     }
 
     /**
@@ -29,7 +45,18 @@ class CreateChirpContext implements Context
      */
     public function iPostTheChirp()
     {
-        throw new PendingException();
+        $this->uuid = $this->faker->uuid;
+        $obj  = (object)[
+            'data' => (object)[
+                'type' => 'chirp',
+                'id' => $this->uuid,
+                'attributes' => (object)[
+                    'text' => $this->chirpText
+                ]
+            ]
+        ];
+
+        $this->httpClient->post('chirp', ['json' => $obj]);
     }
 
     /**
