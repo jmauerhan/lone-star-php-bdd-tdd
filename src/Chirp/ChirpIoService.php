ab0708141c1275888d182260bd1d3b2662a4d517
Jessica Mauerhan <jessicamauerhan@gmail.com>
2017-04-20 10:56:10 -0500

    created Collaborators and required them in service

--- src/Chirp/ChirpIoService.php
+++ src/Chirp/ChirpIoService.php
@@ -7,6 +7,17 @@ use Chirper\Http\Response;
 
 class ChirpIoService
 {
+    /** @var JsonChirpTransformer */
+    private $jsonChirpTransformer;
+    /** @var ChirpPersistenceDriver */
+    private $persistenceDriver;
+
+    public function __construct(JsonChirpTransformer $jsonChirpTransformer, ChirpPersistenceDriver $persistenceDriver)
+    {
+        $this->jsonChirpTransformer = $jsonChirpTransformer;
+        $this->persistenceDriver    = $persistenceDriver;
+
+    }
 
     public function create(Request $request): Response
     {
